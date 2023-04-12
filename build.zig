6b4df4df5aac14f655879b9bce02675f6a7dd6ad
LordMZTE <lord@mzte.de>
2023-04-12 21:08:32 +0200

    port to package manager

--- build.zig
+++ build.zig
@@ -4,47 +4,36 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&testStep(b, optimize, target).step);
-}
-
-var _module: ?*std.build.Module = null;
-
-pub fn module(b: *std.Build) *std.build.Module {
-    if (_module) |m| return m;
-    _module = b.createModule(.{
-        .source_file = .{ .path = sdkPath("/src/main.zig") },
+    const dusk_mod = b.addModule("mach-dusk", .{
+        .source_file = .{ .path = "src/main.zig" },
     });
-    return _module.?;
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&testStep(b, dusk_mod, optimize, target).step);
 }
 
-pub fn testStep(b: *std.Build, optimize: std.builtin.OptimizeMode, target: std.zig.CrossTarget) *std.build.RunStep {
+pub fn testStep(
+    b: *std.Build,
+    dusk_mod: *std.Build.Module,
+    optimize: std.builtin.OptimizeMode,
+    target: std.zig.CrossTarget,
+) *std.build.RunStep {
     const lib_tests = b.addTest(.{
         .name = "dusk-lib-tests",
-        .root_source_file = .{ .path = sdkPath("/src/main.zig") },
+        .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    lib_tests.install();
 
     const main_tests = b.addTest(.{
         .name = "dusk-tests",
-        .root_source_file = .{ .path = sdkPath("/test/main.zig") },
+        .root_source_file = .{ .path = "test/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    main_tests.addModule("dusk", module(b));
-    main_tests.install();
+    main_tests.addModule("dusk", dusk_mod);
 
     const run_step = main_tests.run();
     run_step.step.dependOn(&lib_tests.run().step);
     return main_tests.run();
-}
-
-fn sdkPath(comptime suffix: []const u8) []const u8 {
-    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
-    return comptime blk: {
-        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
-        break :blk root_dir ++ suffix;
-    };
 }
