e7a081161d56edcc93eb6b8ff502be2359c1abdd
Ali Chraghi <alichraghi@proton.me>
2023-08-03 09:50:18 +0330

    update to latest Zig API

--- examples/triangle/main.zig
+++ examples/triangle/main.zig
@@ -1,88 +1,85 @@
 const std = @import("std");
 const dusk = @import("dusk");
-const mach = @import("core");
-const gpu = mach.gpu;
+const core = @import("core");
+const gpu = core.gpu;
 
-pub const App = @This();
+pub const App = struct {
+    pipeline: *gpu.RenderPipeline,
+    queue: *gpu.Queue,
 
-pub const GPUInterface = dusk.Interface;
+    pub const GPUInterface = dusk.Interface;
 
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    pub fn init(app: *App) !void {
+        try core.init(.{});
 
-core: mach.Core,
-pipeline: *gpu.RenderPipeline,
-queue: *gpu.Queue,
+        const shader_module = core.device.createShaderModuleWGSL("shader.wgsl", @embedFile("shader.wgsl"));
 
-pub fn init(app: *App) !void {
-    try app.core.init(gpa.allocator(), .{});
-
-    const shader_module = app.core.device().createShaderModuleWGSL("shader.wgsl", @embedFile("shader.wgsl"));
-
-    // Fragment state
-    const blend = gpu.BlendState{};
-    const color_target = gpu.ColorTargetState{
-        .format = app.core.descriptor().format,
-        .blend = &blend,
-        .write_mask = gpu.ColorWriteMaskFlags.all,
-    };
-    const fragment = gpu.FragmentState.init(.{
-        .module = shader_module,
-        .entry_point = "fragment_main",
-        .targets = &.{color_target},
-    });
-    const pipeline_descriptor = gpu.RenderPipeline.Descriptor{
-        .fragment = &fragment,
-        .vertex = gpu.VertexState{
+        // Fragment state
+        const blend = gpu.BlendState{};
+        const color_target = gpu.ColorTargetState{
+            .format = core.descriptor.format,
+            .blend = &blend,
+            .write_mask = gpu.ColorWriteMaskFlags.all,
+        };
+        const fragment = gpu.FragmentState.init(.{
             .module = shader_module,
-            .entry_point = "vertex_main",
-        },
-    };
+            .entry_point = "fragment_main",
+            .targets = &.{color_target},
+        });
+        const pipeline_descriptor = gpu.RenderPipeline.Descriptor{
+            .fragment = &fragment,
+            .vertex = gpu.VertexState{
+                .module = shader_module,
+                .entry_point = "vertex_main",
+            },
+        };
 
-    app.pipeline = app.core.device().createRenderPipeline(&pipeline_descriptor);
-    app.queue = app.core.device().getQueue();
+        app.pipeline = core.device.createRenderPipeline(&pipeline_descriptor);
+        app.queue = core.device.getQueue();
 
-    shader_module.release();
-}
-
-pub fn deinit(app: *App) void {
-    defer _ = gpa.deinit();
-    defer app.core.deinit();
-}
-
-pub fn update(app: *App) !bool {
-    var iter = app.core.pollEvents();
-    while (iter.next()) |event| {
-        switch (event) {
-            .close => return true,
-            else => {},
-        }
+        shader_module.release();
     }
 
-    const back_buffer_view = app.core.swapChain().getCurrentTextureView().?;
-    const color_attachment = gpu.RenderPassColorAttachment{
-        .view = back_buffer_view,
-        .clear_value = std.mem.zeroes(gpu.Color),
-        .load_op = .clear,
-        .store_op = .store,
-    };
+    pub fn deinit(app: *App) void {
+        core.deinit();
+        _ = app;
+    }
 
-    const encoder = app.core.device().createCommandEncoder(null);
-    const render_pass_info = gpu.RenderPassDescriptor.init(.{
-        .color_attachments = &.{color_attachment},
-    });
-    const pass = encoder.beginRenderPass(&render_pass_info);
-    pass.setPipeline(app.pipeline);
-    pass.draw(3, 1, 0, 0);
-    pass.end();
-    pass.release();
+    pub fn update(app: *App) !bool {
+        var iter = core.pollEvents();
+        while (iter.next()) |event| {
+            switch (event) {
+                .close => return true,
+                else => {},
+            }
+        }
 
-    var command = encoder.finish(null);
-    encoder.release();
+        const back_buffer_view = core.swap_chain.getCurrentTextureView().?;
+        const color_attachment = gpu.RenderPassColorAttachment{
+            .view = back_buffer_view,
+            .clear_value = std.mem.zeroes(gpu.Color),
+            .load_op = .clear,
+            .store_op = .store,
+        };
 
-    app.queue.submit(&[_]*gpu.CommandBuffer{command});
-    command.release();
-    app.core.swapChain().present();
-    back_buffer_view.release();
+        const encoder = core.device.createCommandEncoder(null);
+        const render_pass_info = gpu.RenderPassDescriptor.init(.{
+            .color_attachments = &.{color_attachment},
+        });
+        const pass = encoder.beginRenderPass(&render_pass_info);
+        pass.setPipeline(app.pipeline);
+        pass.draw(3, 1, 0, 0);
+        pass.end();
+        pass.release();
 
-    return false;
-}
+        var command = encoder.finish(null);
+        encoder.release();
+
+        app.queue.submit(&[_]*gpu.CommandBuffer{command});
+        command.release();
+        core.swap_chain.present();
+        back_buffer_view.release();
+
+        return false;
+    }
+};
