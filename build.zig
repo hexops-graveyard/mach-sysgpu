756bb0f81001855aea3af68c584751e85e8d4512
Patrick Doane <pdoane2@gmail.com>
2023-09-19 12:40:48 -0700

    all: test backends

--- build.zig
+++ build.zig
@@ -63,18 +63,42 @@ pub fn build(b: *std.Build) !void {
     try @import("mach_gpu").link(gpu_dep.builder, triangle, .{}); // link dawn
     b.installArtifact(triangle);
 
-    const run_traingle_cmd = b.addRunArtifact(triangle);
+    const run_triangle_cmd = b.addRunArtifact(triangle);
     const run_triangle_step = b.step("triangle", "Run the basic init example");
-    run_triangle_step.dependOn(&run_traingle_cmd.step);
+    run_triangle_step.dependOn(&run_triangle_cmd.step);
 
-    const shader_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/shader.zig" },
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(shader_tests);
+    tests.addModule("vulkan", vulkan_mod);
+    tests.addModule("gpu", gpu_mod);
+    tests.addModule("objc", objc_mod);
 
-    const run_shader_tests = b.addRunArtifact(shader_tests);
+    if (target.isDarwin()) {
+        tests.linkFramework("AppKit");
+        tests.linkFramework("CoreGraphics");
+        tests.linkFramework("Foundation");
+        tests.linkFramework("Metal");
+        tests.linkFramework("QuartzCore");
+    }
+
+    if (target.isWindows()) {
+        tests.addCSourceFile(.{ .file = .{ .path = "src/d3d12/workarounds.c" }, .flags = &.{} });
+
+        tests.linkLibrary(b.dependency("direct3d_headers", .{
+            .target = target,
+            .optimize = optimize,
+        }).artifact("direct3d-headers"));
+        @import("direct3d_headers").addLibraryPath(tests);
+        tests.linkSystemLibrary("d3d12");
+        tests.linkSystemLibrary("d3dcompiler_47");
+    }
+
+    b.installArtifact(tests);
+
+    const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_shader_tests.step);
+    test_step.dependOn(&run_tests.step);
 }
