2aabb2ac634e860bdc89b3009a79831f1f5f7e41
Patrick Doane <pdoane2@gmail.com>
2023-10-03 13:05:23 -0700

    {d3d12,vk}: fix setIndexBuffer return type

--- src/vulkan.zig
+++ src/vulkan.zig
@@ -2024,7 +2024,7 @@ pub const RenderPassEncoder = struct {
         vkd.cmdEndRenderPass(encoder.encoder.buffer.buffer);
     }
 
-    pub fn setIndexBuffer(encoder: *RenderPassEncoder, buffer: *Buffer, format: dgpu.IndexFormat, offset: u64, size: u64) void {
+    pub fn setIndexBuffer(encoder: *RenderPassEncoder, buffer: *Buffer, format: dgpu.IndexFormat, offset: u64, size: u64) !void {
         _ = size;
         _ = offset;
         _ = format;
